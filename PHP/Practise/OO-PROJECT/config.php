047a8acb19710bc5715ea409f11109d3edaffe7c
Komal <komalmulik@benchmarkit.solutions>
2021-11-26 07:23:37 +0530

    25-11

---
+++ PHP/Practise/OO-PROJECT/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+// DEFINE URL
+define("ROOT_PATH", "/OO-PHP/OO-PROJECT/");
+define("ROOT_URL", "http://localhost/OO-PHP/OO-PROJECT/");
+
+?>
